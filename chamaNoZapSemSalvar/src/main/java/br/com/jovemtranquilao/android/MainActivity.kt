b623f5ece3e7ef47d450e9250441120904bf6d81
jovem tranquilao <jovem.tranquilao.2023@gmail.copm>
2023-10-10 19:41:30 -0300

    Feito intent para abrir outro app, no caso whatsapp ou chrome por enquanto

--- chamaNoZapSemSalvar/src/main/java/br/com/jovemtranquilao/android/MainActivity.kt
+++ chamaNoZapSemSalvar/src/main/java/br/com/jovemtranquilao/android/MainActivity.kt
@@ -1,16 +1,23 @@
 package br.com.jovemtranquilao.android
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import br.com.jovemtranquilao.Greeting
-//https://kotlinlang.org/docs/multiplatform-mobile-create-first-app.html#examine-the-project-structure
 
+
+//https://kotlinlang.org/docs/multiplatform-mobile-create-first-app.html#examine-the-project-structure
+// Material components 3
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +27,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    GreetingView(Greeting().greet(), this)
+
+
                 }
             }
         }
@@ -28,14 +37,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+fun GreetingView(text: String, context: ComponentActivity) {
+    Button(onClick = {
+/*
+        val sendIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "Hello Swapz")
+            putExtra("jid", "123132@s.whatsapp.net")
+            type = "text/plain"
+            setPackage("com.whatsapp")
+        }
+
+        context.startActivity(sendIntent)
+*/
+        val urlString = "http://wa.me/5566890987678"
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(urlString))
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.setPackage("com.android.chrome")
+        try {
+            context.startActivity(intent)
+        } catch (ex: ActivityNotFoundException) {
+            // Chrome browser presumably not installed so allow user to choose instead
+            intent.setPackage(null)
+            context.startActivity(intent)
+        }
+    }) { Text("Teste") }
 }
 
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
-    }
-}
+
