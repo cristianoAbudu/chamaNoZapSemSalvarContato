f2198c987cc61f1cf7aadec1921dfdf98a77ff0a
jovem tranquilao <jovem.tranquilao.2023@gmail.copm>
2023-10-17 19:45:36 -0300

    Criando a view do iphone no Swift ui

--- chamaNoZapSemSalvar/chamaNoZapSemSalvar/ContentView.swift
+++ chamaNoZapSemSalvar/chamaNoZapSemSalvar/ContentView.swift
@@ -2,15 +2,42 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greet()
 
+    @State private var telefone: String = ""
+    
 	var body: some View {
-		Text(greet)
         
+        var text = ""
+
         TextField(
-            
+            "User name (email address)",
+            text: $telefone
         )
+        .disableAutocorrection(true)
+        
+        Button("Greeting") {
+            openWhatsapp()
+        }
+        
 	}
+}
+
+func openWhatsapp(){
+    let urlWhats = "whatsapp://send?phone=(mobile number with country code)"
+    if let urlString = urlWhats.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed){
+        if let whatsappURL = URL(string: urlString) {
+            if UIApplication.shared.canOpenURL(whatsappURL){
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(whatsappURL, options: [:], completionHandler: nil)
+                } else {
+                    UIApplication.shared.openURL(whatsappURL)
+                }
+            }
+            else {
+                print("Install Whatsapp")
+            }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
